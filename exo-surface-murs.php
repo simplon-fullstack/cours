b6b40ce821a13289f9edff66b073eb5ef04dfa6b
Your Name <you@example.com>
2019-09-09 14:25:14 +0200

    pour synchro push

---
+++ exo-surface-murs.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+<section>
+        <h3>EXERCICE FONCTION: CALCULER PRIX TTC</h3>
+<?php
+
+
+?>
+</section>
+    <script>
+    </script>    
+</body>
+</html>
