80920723bebf85a732dcb6021d76f747304ce478
Your Name <you@example.com>
2019-09-09 14:33:23 +0200

    corigé exo surface murs

--- exo-surface-murs.php
+++ exo-surface-murs.php
@@ -11,11 +11,46 @@
 <section>
         <h3>EXERCICE FONCTION: CALCULER PRIX TTC</h3>
 <?php
+function calculerSurfaceMurs ($hauteur, $largeur, $longueur)
+{
+    $surface = 2 * ($largeur + $longueur) * $hauteur;
+    return $surface;
+}
 
+// IMPORTANT: L'ORDRE DES VALEURS EST IMPORTANT
+$resultat = calculerSurfaceMurs(2, 3, 4);
+echo "<h4>le prix TTC est $resultat</h4>";
+
+$resultat = calculerSurfaceMurs(5, 6, 7);
+echo "<h4>le prix TTC est $resultat</h4>";
 
 ?>
 </section>
     <script>
+
+// Créer une fonction qui prend en paramètre 3 paramètres
+// * hauteur
+// * largeur
+// * longueur
+// La fonction doit calculer la surface des 4 murs
+
+// EN JS
+function calculerSurfaceMurs (hauteur, largeur, longueur)
+{
+    // AJOUTER LE CODE MANQUANT
+    surface = (longueur + largeur) * 2 * hauteur;
+    // surface =    (largeur  * hauteur) 
+    //            + (longueur * hauteur) 
+    //            + (largeur  * hauteur) 
+    //            + (longueur * hauteur);
+
+    return surface;
+}
+
+// TESTER AVEC 
+// IMPORTANT: L'ORDRE DES VALEURS EST IMPORTANT
+console.log(calculerSurfaceMurs(2, 3, 4));      // 28
+console.log(calculerSurfaceMurs(5, 6, 7));      // 130        
     </script>    
 </body>
 </html>
